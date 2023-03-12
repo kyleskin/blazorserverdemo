9bacfba7c9b819a5fbe7222ce78d9d1ed87cc0b6
Kyle Skinner <kyleskin@gmail.com>
2023-03-11 20:29:30 -0700

    clean up tests

--- BlazorServerDemoTests/PageTests/TodoListPageUnitTests.cs
+++ BlazorServerDemoTests/PageTests/TodoListPageUnitTests.cs
@@ -7,55 +7,59 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace BlazorServerDemoTests.PageTests;
 
-public class TodoListPageUnitTests : IDisposable
+public class TodoListPageUnitTests
 {
-    private readonly TestContext _testContext;
-    private Mock<ITodoService> _mockTodos;
     private readonly List<Todo> _todos = new()
     {
         new() { Title = "Prepare presentation" },
         new() { Title = "Commit changes" },
         new() { Title = "Log time" }
     };
-
+    
     public TodoListPageUnitTests()
     {
-        _testContext = new TestContext();
-        _mockTodos = new();
         _todos[0].MarkComplete();
-        _testContext.Services.AddSingleton(_mockTodos.Object);
     }
-
-    public void Dispose()
-    {
-        _testContext.Dispose();
-    }
-
+    
     [Fact]
     public void PageInstance_ShouldLoadOnlyIncompleteTodos_WhenPageLoaded()
     {
-        _mockTodos.Setup(t => t.GetIncompleteTodosAsync())
+        // Arrange
+        var mockTodos = new Mock<ITodoService>();
+        mockTodos.Setup(t => t.GetIncompleteTodosAsync())
             .ReturnsAsync(_todos.Where(t => !t.IsComplete));
-        var page = _testContext.RenderComponent<TodoList>();
         
-        var todos = page.Instance.Todos;
+        using var ctx = new TestContext();
+        ctx.Services.AddSingleton(mockTodos.Object);
+        var cut = ctx.RenderComponent<TodoList>();
+        
+        // Act
+        var todos = cut.Instance.Todos;
 
-        _mockTodos.Verify(x => x.GetIncompleteTodosAsync(), Times.Once);
+        // Assert
+        mockTodos.Verify(x => x.GetIncompleteTodosAsync(), Times.Once);
         todos.Should().NotBeEmpty()
-            .And.NotContain(t => t.IsComplete);
+             .And.NotContain(t => t.IsComplete);
     }
 
     [Fact]
     public async void ShowAllTodos_ShouldLoadAllTodos_WhenCalled()
     {
-        _mockTodos.Setup(t => t.GetTodosAsync())
-            .ReturnsAsync(_todos);
-        var page = _testContext.RenderComponent<TodoList>();
+        // Arrange
+        var mockTodos = new Mock<ITodoService>();
+        mockTodos.Setup(t => t.GetTodosAsync())
+                 .ReturnsAsync(_todos);
         
-        await page.Instance.ShowAllTodos();
-        var todos = page.Instance.Todos;
+        using var ctx = new TestContext();
+        ctx.Services.AddSingleton(mockTodos.Object);
+        var cut = ctx.RenderComponent<TodoList>();
+        
+        // Act
+        await cut.Instance.ShowAllTodos();
+        var todos = cut.Instance.Todos;
 
-        _mockTodos.Verify(m => m.GetTodosAsync(), Times.Once);
+        // Assert
+        mockTodos.Verify(m => m.GetTodosAsync(), Times.Once);
         todos.Should().BeEquivalentTo(_todos);
     }
 }
